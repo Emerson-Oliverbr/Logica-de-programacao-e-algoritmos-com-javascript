4b8c0dd5c60b1c4b0a1dc93268d7f89fab95ad88
Emerson Oliveira <emerson.oliverbr@gmail.com>
2026-07-13 20:44:42 -0300

    Tipos de dados

---
+++ Modulo-1-Introducao/3.Tipos-de-dados/index.js
@@ -0,0 +1,30 @@
+//Tipos primitivos 
+
+//number
+let numero = 10;
+let numeroQuebrado = 1.5;
+console.log(`A variavel ${numero} e do tipo ${typeof(numero)}`);
+console.log(`A variavel ${numeroQuebrado} e do tipo ${typeof(numeroQuebrado)}`);
+
+//string
+let name = "Emerson Oliveira";
+//bool
+let ligado = true;
+
+//undefined 
+let fullName;
+console.log(`A variavel ${fullName} e do tipo ${typeof(fullName)}`);
+
+//null
+let LigadoEDesligado = null;
+console.log(`A variavel ${LigadoEDesligado} e do tipo ${typeof(LigadoEDesligado)}`);
+
+//object
+let typeObject = {}; 
+console.log(`A variavel ${typeObject} e do tipo ${typeof(typeObject)}`);
+
+let typeFunction = () => {};
+console.log(`A variavel ${typeFunction} e do tipo ${typeof(typeFunction)}`);
+
+
+
