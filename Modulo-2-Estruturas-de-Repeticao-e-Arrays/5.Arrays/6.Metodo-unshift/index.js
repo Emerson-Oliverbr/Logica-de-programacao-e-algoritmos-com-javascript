3741706ccd8b42f90276bd58964c7096c11f96d7
Emerson Oliveira <emerson.oliverbr@gmail.com>
2026-07-22 19:27:03 -0300

    Metodo unshift

---
+++ Modulo-2-Estruturas-de-Repeticao-e-Arrays/5.Arrays/6.Metodo-unshift/index.js
@@ -0,0 +1,21 @@
+const frutas = ["Morango", "Laranja", "Lomão", "Kiwi"];
+console.log(frutas);
+
+frutas.push("Uva");
+console.log(frutas);
+
+frutas.push("Manga");
+console.log(frutas);
+
+frutas.push("Morango");
+console.log(frutas);
+
+frutas.unshift("Mamão");
+console.log(frutas);
+
+frutas.unshift("Abacaxi");
+console.log(frutas);
+
+frutas.unshift("Mamão");
+console.log(frutas);
+
